74f9dc580f3da799aabf0f506910b45825eecc00
SoheilRezaei <soheilrezaei966+github@gmail.com>
2024-04-16 20:57:05 -0400

    Added css modules to components

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 export default function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h2>Homepage</h2>
+
+      <Link to="/app">Go to app</Link>
     </div>
   );
 }
