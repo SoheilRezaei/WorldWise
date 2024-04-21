d2cfdb97ab65c5853ff6e8bc659b20c9e62c0f6f
SoheilRezaei <soheilrezaei966+github@gmail.com>
2024-04-21 19:19:24 -0400

    Re-written the project and added css modules to different pages

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,10 +1,23 @@
-import PageNav from "../components/PageNav";
+import styles from "./Product.module.css";
 
-export default function Pricing() {
+export default function Product() {
   return (
-    <div>
-      <PageNav />
-      <h2>Pricing</h2>
-    </div>
+    <main className={styles.product}>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
   );
 }
