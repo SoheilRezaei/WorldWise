f57815d894d387df7fc8632f418f271de701a1de
SoheilRezaei <soheilrezaei966+github@gmail.com>
2024-04-29 20:29:53 -0400

    added the navigation to all other pages

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,8 +1,10 @@
+import PageNav from "../components/PageNav";
 import styles from "./Product.module.css";
 
 export default function Product() {
   return (
     <main className={styles.product}>
+      <PageNav />
       <section>
         <div>
           <h2>
